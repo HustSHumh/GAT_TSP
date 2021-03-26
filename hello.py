c4172d9acf5125343ecef2869c828000afcd2f0d
Shu Minhao <a495650347@qq.com>
2021-03-26 13:12:28 +0800

    lite transformer

--- hello.py
+++ hello.py
@@ -6,9 +6,12 @@ import numpy as np
 
 if __name__ == '__main__':
     a = torch.randn(10, 20, 2)
-    x = a[:, :, 0]
-    y = a[:, :, 1]
-    print(F.pairwise_distance(x, y))
+    b = torch.randn(10, 20, 2)
+    b = torch.sigmoid(b)
+    print((a * b).size())
+    # x = a[:, :, 0]
+    # y = a[:, :, 1]
+    # print(F.pairwise_distance(x, y))
 
 
 
