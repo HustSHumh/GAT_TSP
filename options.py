2095964e70e1ec3bbad0b060d65f4611e937649c
Shu Minhao <a495650347@qq.com>
2021-04-01 10:11:36 +0800

    only reduce GAT dim

--- options.py
+++ options.py
@@ -11,7 +11,7 @@ def get_options(args=None):
     # Data
     parser.add_argument('--problem', default='tsp', help="The problem to solve, default 'tsp'")
     parser.add_argument('--graph_size', type=int, default=20, help="The size of the problem graph")
-    parser.add_argument('--batch_size', type=int, default=1024, help='Number of instances per batch during training')
+    parser.add_argument('--batch_size', type=int, default=512, help='Number of instances per batch during training')
     parser.add_argument('--epoch_size', type=int, default=1280000, help='Number of instances per epoch during training')
     parser.add_argument('--val_size', type=int, default=10000,
                         help='Number of instances used for reporting validation performance')
